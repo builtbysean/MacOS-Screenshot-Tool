ecb365cc2f02de9d8eafe5a9930a8b13fb9ab73a
builtbysean <seanoffin2003@gmail.com>
2025-07-13 14:58:34 -0400

    Added menu bar and settings for screenshots

---
+++ ScreenshotApp/View/MenuBar/MenuBarContentView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBarContentView.swift
+//  ScreenshotApp
+//
+//  Created by Sean Coffin on 7/13/25.
+//
+
+import SwiftUI
+
+struct MenuBarContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuBarContentView()
+}
